00894d68612f6fd0ebd19cd59367a71a57a43e72
imahdimir <imahdimir@gmail.com>
2022-09-07 20:52:48 +0430

    v1

--- main.py
+++ main.py
@@ -2,41 +2,56 @@
 
   """
 
-##
-
-
 import json
 from ast import literal_eval
 
 import requests
+from githubdata import GithubData
 
+
+class GDUrl :
+    trg = 'https://github.com/imahdimir/rd-tse_ir-industry-subIndustry'
+    cur = 'https://github.com/imahdimir/u-rd-tse_ir-industry-subIndustry'
+
+gdu = GDUrl()
 
 def main() :
+    pass
 
-  pass
+    ##
+    url = 'https://tse.ir/json/Listing/ListingByGroup1.json'
+    ##
+    r = requests.get(url , verify = False)
+    t1 = r.text
+    ##
+    t1 = '{' + t1.split('{' , 1)[1]
+    ##
+    dc = literal_eval(t1)
+    ##
 
-  ##
-  url = 'https://tse.ir/json/Listing/ListingByGroup1.json'
-  ##
-  r = requests.get(url , verify = False)
-  t1 = r.text
-  ##
-  t1 = '{' + t1.split('{' , 1)[1]
-  ##
-  dc = literal_eval(t1)
-  ##
-  with open('sc.json' , 'w' , encoding = 'utf8') as fi :
-    json.dump(dc , fi)
+    gd_trg = GithubData(gdu.trg)
+    gd_trg.overwriting_clone()
+    ##
+    jsp = gd_trg.local_path / 'data.json'
+    ##
+    with open(jsp , 'w' , encoding = 'utf8') as fi :
+        json.dump(dc , fi)
+    ##
+    msg = 'update data.json by: '
+    msg += gdu.cur
+    ##
+    gd_trg.commit_and_push(msg)
+    ##
 
-  ##
-  with open('sc.json' , 'r') as fi :
-    dc1 = json.load(fi)
 
-  ##
+    gd_trg.rmdir()
+
+
+    ##
 
 ##
 if __name__ == '__main__' :
-  main()
+    main()
 
 ##
 
